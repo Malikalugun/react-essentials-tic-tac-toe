bdc0ce4e20cf294dfa53702f652479266d6b6f13
Malikalugun <105351482+Malikalugun@users.noreply.github.com>
2024-02-17 22:32:39 +0530

    tic tac toe

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <main>
+    <div id="game-container">
+<ol id="players">
+<li>
+  <span className="player-name">Player 1</span>
+  <span className='player-symbol'>X</span>
+</li>
+<li>
+  <span className="player-name">Player 2</span>
+  <span className='player-symbol'>O</span>
+</li>
+</ol>
+    </div>
+   </main>
+  )
+}
+
+export default App
